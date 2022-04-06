fe76ca1e28adf82a7d2a498a6a5275f2db93f007
Ygor Guilherme <ygorfsguilherme@gmail.com>
2022-04-06 10:28:45 -0300

    fixeding header and footer loading on pages

--- assets/js/modules/loadHtml.js
+++ assets/js/modules/loadHtml.js
@@ -3,15 +3,18 @@ function loadTemplate(file, local){
 
   let rawFile = new XMLHttpRequest();
   rawFile.open("GET", file, false);
+
   rawFile.onreadystatechange = function (){
     if(rawFile.readyState === 4){
       if(rawFile.status === 200 || rawFile.status == 0){
         addTemplate.innerHTML = rawFile.responseText;
-      }
+      };
+
     }
   }
+
   rawFile.send(null);
 }
 
-loadTemplate('page/templates/header.html', '[data-header]');
-loadTemplate('page/templates/footer.html', '[data-footer]');
+loadTemplate('templates/header.html', '[data-header]');
+loadTemplate('templates/footer.html', '[data-footer]');
