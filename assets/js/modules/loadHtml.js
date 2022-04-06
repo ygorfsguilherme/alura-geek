4c90dfb8921d4d3898f09bbe0e7f83e8a537d060
Ygor Guilherme <ygorfsguilherme@gmail.com>
2022-04-06 08:03:45 -0300

    fixeding loading header and footer

--- assets/js/modules/loadHtml.js
+++ assets/js/modules/loadHtml.js
@@ -1,10 +1,17 @@
-function loadTemplate(template, local){
-	let addTemplate = document.querySelector(`${local}`)
+function loadTemplate(file, local){
+  let addTemplate = document.querySelector(`${local}`);
 
-	fetch(template)
-	.then( r => r.text() )
-	.then( t => addTemplate.innerHTML = t);
+  let rawFile = new XMLHttpRequest();
+  rawFile.open("GET", file, false);
+  rawFile.onreadystatechange = function (){
+    if(rawFile.readyState === 4){
+      if(rawFile.status === 200 || rawFile.status == 0){
+        addTemplate.innerHTML = rawFile.responseText;
+      }
+    }
+  }
+  rawFile.send(null);
 }
 
-loadTemplate('../../page/templates/header.html', '[data-header]');
-loadTemplate('../../page/templates/footer.html', '[data-footer]');
+loadTemplate('../../../page/templates/header.html', '[data-header]');
+loadTemplate('../../../page/templates/footer.html', '[data-footer]');
